57efbe37c16ff99371019b24e1feb38b23319c76
rjo21 <robinsonor4@gmail.com>
2023-06-07 17:06:22 -0500

    patrocinadores

--- prueba-landingpages/src/components/Patrocinadores.js
+++ prueba-landingpages/src/components/Patrocinadores.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import '../styles/Patrocinadores.css';
+import Image from 'next/image';
+import aguila from '../assets/aguila.jpg'
+import Envia from '../assets/envia.png'
+import Olimpica from '../assets/olimpica.png'
+import tecnoglass from '../assets/tecnoglass.png'
+import Aguilacerveza from '../assets/aguilacerveza.jpg'
+
 
 const Patrocinadores = () => {
     return(
         <>
         <div className='slider-patrocinadores'>
+            <h1>Patrocinadores</h1>
             <div className='slider-track'>
             <div className='slide-patrocinador'>
-                <Image />
+                <Image src={aguila}/>
             </div>
             <div className='slide-patrocinador'>
-                <Image />
+                <Image src={tecnoglass}/>
             </div>
             <div className='slide-patrocinador'>
-                <Image />
+                <Image src={Aguilacerveza}/>
             </div>
             <div className='slide-patrocinador'>
-                <Image />
+                <Image src={Envia}/>
             </div><div className='slide-patrocinador'>
-                <Image />
+                <Image src={Olimpica}/>
             </div>
             </div>
         </div>
